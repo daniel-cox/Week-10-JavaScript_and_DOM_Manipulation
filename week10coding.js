0a1bfa1d8d1d784651dfc8b9189da2578efbfe55
daniel-cox <ghostseal3@gmail.com>
2023-02-03 00:06:00 -0600

    added code changes

--- week10coding.js
+++ week10coding.js
@@ -1,27 +1,44 @@
 let id = 0;
 
+//NOTE -c
 document.getElementById("add").addEventListener("click", () => {
-  id++;
+  id++; //incrementing the id at the beginning of the function
+
+  //NOTE -  setting variables to pull data from the Input labels
   let table = document.getElementById("list");
   let row = table.insertRow(1);
   let carwashNotes = document.getElementById("washNotes").value;
   let carWashDate = document.getElementById("new-carWash").value;
   let carWashPrice = document.getElementById("washPrice").value;
-  row.setAttribute("id", `item-${id}`);
+
+  //NOTE - sets the table row with the data passed form the Input labels
+
+  row.setAttribute("id", `item-${id}`); // Sets the value of the incremented ID
+
+  //NOTE - Inserts various cells containg HTML from the input labels
   row.insertCell(0).innerHTML = id;
   row.insertCell(1).innerHTML = carWashDate;
   row.insertCell(2).innerHTML = carwashNotes;
   row.insertCell(3).innerHTML = carWashPrice;
   row.insertCell(4).innerHTML = `
+
   <button type="button" id ="deleteCar-${id}" class="btn btn-danger">Delete</button>`;
+  //NOTE -clears the input field
   document.getElementById("new-carWash").value = "";
-  document.getElementById(`deleteCar-${id}`).addEventListener("click", () => {
-    console.log("deleting row..", `item-${id}`);
-    let row = document.getElementById(`item-${id}`);
-    let rowIndex = row.rowIndex;
-    console.log(rowIndex);
-    if (rowIndex === 0) {
-      console.log("the header stays");
-    } else table.deleteRow(rowIndex - 1);
-  });
+
+  //NOTE -
+  document
+    .getElementById(`deleteCar-${id}`)
+    .addEventListener("click", (event) => {
+      //console logs the test code
+      console.log("logging event", event.target.parentNode.parentNode);
+      console.log("deleting row..", `item-${id}`);
+      //NOTE -variable for deleting the row
+
+      //NOTE - Looks at the event listener, the target property, the parent node (td) , and the nested parent node (tr)
+      let row = event.target.parentNode.parentNode;
+
+      //NOTE - removes the row
+      row.remove();
+    });
 });
